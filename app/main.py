5f84f8a6e97dcbb965888f41e003bb5d326b1c0b
bohn13 <bod3171@gmail.com>
2026-02-07 01:23:39 +0100

    Completed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    temp = command.split()
+
+    if len(temp) != 3:
+        return
+
+    cp, src, trg = temp
+
+    if cp != "cp":
+        return
+
+    try:
+        with (open(src, "r", encoding="utf-8") as file,
+              open(trg, "w", encoding="utf-8") as out):
+            out.write(file.read())
+    except FileNotFoundError:
+        return
